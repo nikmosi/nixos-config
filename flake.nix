062a853fa9f92ae13252bf0a085690fa2516b88c
nikmosi <i@nikmosi.ru>
2025-01-04 18:24:29 +0700

    flake conf from gpt

--- flake.nix
+++ flake.nix
@@ -1,15 +1,30 @@
 {
-  description = "A very basic flake";
+  description = "My NixOS and Home Manager configurations";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Adjust version as needed
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations = {
+      my-host = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./nixos/configuration.nix
+        ];
+      };
+    };
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    homeConfigurations = {
+      my-user = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        modules = [
+          ./home-manager/home.nix
+        ];
+      };
+    };
   };
 }
+
