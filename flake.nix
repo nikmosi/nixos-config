18e3960ec11bf61b691d9436a60d77731323b51f
nikmosi <i@nikmosi.ru>
2025-01-04 18:27:56 +0700

    change nixpkgs version to 24.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My NixOS and Home Manager configurations";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; # Adjust version as needed
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.11";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
