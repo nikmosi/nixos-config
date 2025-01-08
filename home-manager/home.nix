fdc3cbde81f4bb9bbe3985a03a505e903998cca0
nikmosi <i@nikmosi.ru>
2025-01-08 22:23:03 +0700

    move git config to hm

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -116,6 +116,25 @@
     enable = true;
     userName = "nikmosi";
     userEmail = "i@nikmosi.ru";
+
+    userSigningKey = "B77DD388609E81892CBC2D6AB9CF67BBE64E1273";
+
+    # Git defaults
+    init.defaultBranch = "main";
+    pull.ff = "only";
+    merge.conflictstyle = "zdiff3";
+    merge.tool = "meld";
+    push.default = "current";
+    commit.verbose = true;
+    commit.gpgSign = true;
+    rerere.enabled = true;
+    core.pager = "bat";
+    diff.algorithm = "histogram";
+    url."git@github.com:".insteadOf = "https://github.com/";
+    transfer.fsckObjects = true;
+    fetch.fsckObjects = true;
+    receive.fsckObjects = true;
+    branch.sort = "-committerdate";
   };
 
   # Let Home Manager install and manage itself.
