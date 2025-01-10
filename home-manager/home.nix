70dceedda649410fc308258993bdb193e33a4343
nikmosi <i@nikmosi.ru>
2025-01-10 23:16:35 +0700

    refactor

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,22 +1,10 @@
 { pkgs, config, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
   home.username = "nik";
   home.homeDirectory = "/home/nik";
+  home.stateVersion = "24.11";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "24.11"; # Please read the comment before changing.
-
-  # The home.packages option allows you to install Nix packages into your
-  # environment.
   home.packages = with pkgs; [
     # fonts
     (nerdfonts.override {
@@ -30,51 +18,31 @@
     noto-fonts-emoji
     weather-icons
 
-    #pass
+    # pass
     passff-host
 
+    eza
     fzf
     git
     maim
-    zbar
-    rofi
     pre-commit
-    stow
-    cargo
+    rofi
     source-code-pro
+    stow
+    zbar
     zoxide
-    eza
 
     # lsp
     docker-compose-language-service
-    nil
-    lua-language-server
-    ruff
     docker-ls
-    python312Packages.python-lsp-server
+    lua-language-server
+    nil
     pyright
+    python312Packages.python-lsp-server
     ripdrag
-
-    # # Adds the 'hello' command to your environment. It prints a friendly
-    # # "Hello, world!" when run.
-    # pkgs.hello
-
-    # # It is sometimes useful to fine-tune packages, for example, by applying
-    # # overrides. You can do that directly here, just don't forget the
-    # # parentheses. Maybe you want to install Nerd Fonts with a limited number of
-    # # fonts?
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
-
-    # # You can also create simple shell scripts directly inside your
-    # # configuration. For example, this adds a command 'my-hello' to your
-    # # environment:
-    # (pkgs.writeShellScriptBin "my-hello" ''
-    #   echo "Hello, ${config.home.username}!"
-    # '')
+    ruff
   ];
 
-  # Home Manager is pretty good at managing dotfiles. The primary way to manage
-  # plain files is through 'home.file'.
   home.file = {
     # # Building this configuration will create a copy of 'dotfiles/screenrc' in
     # # the Nix store. Activating the configuration will then make '~/.screenrc' a
@@ -88,22 +56,6 @@
     # '';
   };
 
-  # Home Manager can also manage your environment variables through
-  # 'home.sessionVariables'. These will be explicitly sourced when using a
-  # shell provided by Home Manager. If you don't want to manage your shell
-  # through Home Manager then you have to manually source 'hm-session-vars.sh'
-  # located at either
-  #
-  #  ~/.nix-profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  ~/.local/state/nix/profiles/profile/etc/profile.d/hm-session-vars.sh
-  #
-  # or
-  #
-  #  /etc/profiles/per-user/nik/etc/profile.d/hm-session-vars.sh
-  #
   fonts.fontconfig.enable = true;
 
   programs.password-store = {
@@ -156,6 +108,7 @@
     };
 
   };
+  
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 }
