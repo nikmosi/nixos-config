ced5e21533e9ce8ac94d7ab8d8ba8ab4179a223c
nikmosi <i@nikmosi.ru>
2025-01-26 20:14:08 +0700

    add picom

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -4,4 +4,17 @@
     touchpad.accelProfile = "flat";
     mouse.accelProfile = "flat";
   };
+  services.picom = {
+    enable = true;
+    settings = {
+      backend = "glx";
+      glx-copy-from-front = true;
+      glx-swap-method = 2;
+      glx-use-copysubbuffer-mesa = true;
+      unredir-if-possible = false;
+      vsync = true;
+      xrender-sync = true;
+      xrender-sync-fence = true;
+    };
+  };
 }
