dfb7cd39b2f3607bae4cffb5607150b6c4cacbc7
nikmosi <i@nikmosi.ru>
2025-02-23 15:01:14 +0700

    disable ipv6

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -18,6 +18,7 @@
   # Pick only one of the below networking options.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
   networking.networkmanager.enable = true; # Easiest to use and most distros use this by default.
+  networking.enableIPv6 = false;
 
   # Set your time zone.
   time.timeZone = "Asia/Novosibirsk";
