f626d040ed34264be09f31e44a93363b85a87358
nikmosi <i@nikmosi.ru>
2025-02-23 20:03:33 +0700

    add dev shell

--- flake.nix
+++ flake.nix
@@ -7,15 +7,15 @@
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     ayugram-desktop.url = "github:/ayugram-port/ayugram-desktop/release?submodules=1";
     stylix.url = "github:danth/stylix/release-24.11";
-    git-hooks.url = "github:cachix/git-hooks.nix";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
   };
 
   outputs =
-    {
-      nixpkgs,
-      home-manager,
-      ayugram-desktop,
-      ...
+    { self
+    , nixpkgs
+    , home-manager
+    , ayugram-desktop
+    , ...
     }@inputs:
     let
       system = "x86_64-linux";
@@ -23,16 +23,35 @@
         system = system;
       };
       qtileDeps = import ./qtile-deps.nix { inherit pkgs; };
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
     in
     {
-      checks = {
-        pre-commit-check = inputs.git-hooks.lib.${system}.run {
+      checks = forAllSystems (system: {
+        pre-commit-check = inputs.pre-commit-hooks.lib.${system}.run {
           src = ./.;
           hooks = {
             nixpkgs-fmt.enable = true;
           };
         };
-      };
+      });
+
+      devShells = forAllSystems (system: {
+        default = nixpkgs.legacyPackages.${system}.mkShell {
+          buildInputs = self.checks.${system}.pre-commit-check.enabledPackages;
+          shellHook = ''
+            ${self.checks.${system}.pre-commit-check.shellHook}
+            exec fish
+          '';
+
+        };
+      });
       nixosConfigurations = {
         nixos = nixpkgs.lib.nixosSystem {
           inherit system;
