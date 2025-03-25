e2a5c5d5aa1c566b299f6c5aca1cef96a982c288
nikmosi <i@nikmosi.ru>
2025-03-25 17:09:45 +0700

    chore: remove tmux-sessionx

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -1,26 +1,21 @@
-{ pkgs, unstable, ... }:
+{ pkgs, ... }:
 {
   programs.tmux = {
     enable = true;
     secureSocket = false;
 
-    plugins =
-      with pkgs.tmuxPlugins;
-      [
-        nord
-        tmux-fzf
-        sessionist
-        open
-        sensible
-        vim-tmux-navigator
-        tokyo-night-tmux
-        continuum
-      ]
-      ++ [ unstable.tmuxPlugins.tmux-sessionx ];
+    plugins = with pkgs.tmuxPlugins; [
+      nord
+      tmux-fzf
+      sessionist
+      open
+      sensible
+      vim-tmux-navigator
+      tokyo-night-tmux
+      continuum
+    ];
     historyLimit = 30000;
     extraConfig = ''
-      set -g @sessionx-bind 'o'
-
       set-option -sa terminal-overrides ",xterm*:Tc"
 
       set -g @resurrect-capture-pane-contents 'on'
