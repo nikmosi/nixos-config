a08ca832f8df45e87a4e5d2d74073dd4f0372920
nikmosi <i@nikmosi.ru>
2025-03-27 01:50:21 +0700

    chore(kitty): remove old colors

--- home-manager/modules/kitty.nix
+++ home-manager/modules/kitty.nix
@@ -1,30 +1,6 @@
 { ... }:
 let
   kittyConfig = {
-    # Colors configuration
-    background = "#000000";
-    foreground = "#ebdbb2";
-
-    # Normal colors
-    color0 = "#282828";
-    color1 = "#cc241d";
-    color2 = "#98971a";
-    color3 = "#d79921";
-    color4 = "#458588";
-    color5 = "#b16286";
-    color6 = "#689d6a";
-    color7 = "#d3d3d3";
-
-    # Bright colors
-    color8 = "#928374";
-    color9 = "#fb4934";
-    color10 = "#b8bb26";
-    color11 = "#fabd2f";
-    color12 = "#83a598";
-    color13 = "#d3869b";
-    color14 = "#8ec07c";
-    color15 = "#ebdbb2";
-
     # Cursor settings
     cursor_color = "#ebdbb2";
 
