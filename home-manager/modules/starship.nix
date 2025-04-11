bee30f7d573d5228bb8f44b188f0af5d4203ab90
nikmosi <i@nikmosi.ru>
2025-04-12 01:28:01 +0700

    refactor(starship): simplify formats

--- home-manager/modules/starship.nix
+++ home-manager/modules/starship.nix
@@ -1,3 +1,6 @@
+let
+  commonFormat = ''[$symbol($version)]($style)'';
+in
 {
   programs.starship = {
     enable = true;
@@ -5,276 +8,125 @@
     settings = {
       command_timeout = 1000;
 
+      aws.format = ''\[[$symbol($profile)(\($region\))($duration)]($style)\]'';
+
+      c.format = ''[[$symbol($version(-$name))]($style)]'';
+
+      cmd_duration.format = ''\[[ $duration ]($style)\]'';
+
+      conda.format = ''[[$symbol$environment]($style)]'';
+
+      docker_context.format = ''[[$symbol$context]($style)]'';
+
+      dotnet.format = ''[[$symbol($version)(🎯 $tfm)]($style)]'';
+
+      elixir.format = ''[[$symbol($version (OTP $otp_version))]($style)]'';
+
+      fossil_branch.format = ''[[$symbol$branch]($style)]'';
+
+      gcloud.format = ''[$symbol$account(@$domain)($region)]($style)'';
+
+      git_status.format = ''([\[$all_status$ahead_behind\]]($style))'';
+
+      guix_shell.format = ''[[$symbol]($style)]'';
+
+      hg_branch.format = ''[[$symbol$branch]($style)]'';
+
+      kubernetes.format = ''\[[$symbol$context( \($namespace\))]($style)\]'';
+
+      memory_usage.format = ''[$symbol[$ram( | $swap)]($style)]'';
+
+      meson.format = ''[[$symbol$project]($style)]'';
+
+      ocaml.format = ''\[[$symbol($version)(\($switch_indicator$switch_name\))]($style)\]'';
+
+      openstack.format = ''\[[$symbol$cloud(\($project\))]($style)\]'';
+
+      os.format = ''[[$symbol]($style)]'';
+
+      pijul_channel.format = ''[[$symbol$channel]($style)]'';
+
+      pulumi.format = ''[[$symbol$stack]($style)]'';
+
+      raku.format = ''[[$symbol($version-$vm_version)]($style)]'';
+
+      spack.format = ''[[$symbol$environment]($style)]'';
+
+      sudo.format = ''[[as $symbol]($style)]'';
+
+      terraform.format = ''[[$symbol$workspace]($style)]'';
+
+      time.format = ''[[$time]($style)]'';
+
+      username.format = ''[[$user]($style)]'';
+
       directory = {
-        read_only = "";
-        read_only_style = "fg:#8ABEB7";
-        format = "[$path]($style)[$read_only]($read_only_style) ";
-      };
-
-      aws = {
-        format = "\\[[$symbol($profile)(\\($region\\))($duration)]($style)\\]";
-      };
-
-      bun = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      c = {
-        format = "\\[[$symbol($version(-$name))]($style)\\]";
-      };
-
-      cmake = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      cmd_duration = {
-        format = "\\[[ $duration]($style) \\]";
-      };
-
-      cobol = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      conda = {
-        format = "\\[[$symbol$environment]($style)\\]";
-      };
-
-      crystal = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      daml = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      dart = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      deno = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      docker_context = {
-        format = "\\[[$symbol$context]($style)\\]";
-      };
-
-      dotnet = {
-        format = "\\[[$symbol($version)(🎯 $tfm)]($style)\\]";
-      };
-
-      elixir = {
-        format = "\\[[$symbol($version (OTP $otp_version))]($style)\\]";
-      };
-
-      elm = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      erlang = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      fennel = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      fossil_branch = {
-        format = "\\[[$symbol$branch]($style)\\]";
-      };
-
-      gcloud = {
-        format = "\\[[$symbol$account(@$domain)(\\($region\\))]($style)\\]";
+        read_only = '''';
+        read_only_style = ''fg:#8ABEB7'';
+        format = ''[$path]($style)[$read_only]($read_only_style) '';
       };
 
       git_branch = {
-        symbol = " ";
-        format = "\\[ [$symbol$branch]($style) \\]";
-      };
-
-      git_status = {
-        format = "([\\[$all_status$ahead_behind\\]]($style))";
-      };
-
-      golang = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      gradle = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      guix_shell = {
-        format = "\\[[$symbol]($style)\\]";
-      };
-
-      haskell = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      haxe = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      helm = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      hg_branch = {
-        format = "\\[[$symbol$branch]($style)\\]";
-      };
-
-      java = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      julia = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      kotlin = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      kubernetes = {
-        format = "\\[[$symbol$context( \\($namespace\\))]($style)\\]";
+        symbol = '' '';
+        format = ''\[ [$symbol$branch]($style) \]'';
       };
 
       lua = {
-        format = "\\[[$symbol($version)]($style)\\]";
+        format = commonFormat;
         detect_folders = [ ];
       };
 
-      memory_usage = {
-        format = "\\[$symbol[$ram( | $swap)]($style)\\]";
-      };
-
-      meson = {
-        format = "\\[[$symbol$project]($style)\\]";
-      };
-
-      nim = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
       nix_shell = {
-        impure_msg = "i";
-        symbol = "❄️";
-        pure_msg = "p";
-        unknown_msg = "u";
-        format = "\\[[$symbol $state \\($name\\)]($style)\\]";
-      };
-
-      nodejs = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      ocaml = {
-        format = "\\[[$symbol($version)(\\($switch_indicator$switch_name\\))]($style)\\]";
-      };
-
-      opa = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      openstack = {
-        format = "\\[[$symbol$cloud(\\($project\\))]($style)\\]";
-      };
-
-      os = {
-        format = "\\[[$symbol]($style)\\]";
+        impure_msg = ''i'';
+        symbol = ''❄️'';
+        pure_msg = ''p'';
+        unknown_msg = ''u'';
+        format = ''\[[$symbol $state \($name\)]($style)\]'';
       };
 
       package = {
-        symbol = "󰏗 ";
-        format = "\\[[$symbol$version]($style)\\]";
-      };
-
-      perl = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      php = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      pijul_channel = {
-        format = "\\[[$symbol$channel]($style)\\]";
-      };
-
-      pulumi = {
-        format = "\\[[$symbol$stack]($style)\\]";
-      };
-
-      purescript = {
-        format = "\\[[$symbol($version)]($style)\\]";
+        symbol = ''󰏗 '';
+        format = ''[[$symbol$version]($style)]'';
       };
 
       python = {
-        symbol = " ";
-        format = "\\[[$symbol$pyenv_prefix($version)( \\($virtualenv\\))]($style)\\]";
+        symbol = '' '';
+        format = ''\[[$symbol$pyenv_prefix($version)( \($virtualenv\))]($style)\]'';
       };
 
-      raku = {
-        format = "\\[[$symbol($version-$vm_version)]($style)\\]";
-      };
-
-      red = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      ruby = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      rust = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      scala = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      spack = {
-        format = "\\[[$symbol$environment]($style)\\]";
-      };
-
-      sudo = {
-        format = "\\[[as $symbol]($style)\\]";
-      };
-
-      swift = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      terraform = {
-        format = "\\[[$symbol$workspace]($style)\\]";
-      };
-
-      time = {
-        format = "\\[[$time]($style)\\]";
-      };
-
-      username = {
-        format = "\\[[$user]($style)\\]";
-      };
-
-      vagrant = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      vlang = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      zig = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
-      solidity = {
-        format = "\\[[$symbol($version)]($style)\\]";
-      };
-
+      vagrant.format = commonFormat;
+      vlang.format = commonFormat;
+      zig.format = commonFormat;
+      solidity.format = commonFormat;
+      bun.format = commonFormat;
+      cmake.format = commonFormat;
+      cobol.format = commonFormat;
+      crystal.format = commonFormat;
+      daml.format = commonFormat;
+      dart.format = commonFormat;
+      deno.format = commonFormat;
+      elm.format = commonFormat;
+      erlang.format = commonFormat;
+      fennel.format = commonFormat;
+      golang.format = commonFormat;
+      gradle.format = commonFormat;
+      haskell.format = commonFormat;
+      haxe.format = commonFormat;
+      helm.format = commonFormat;
+      java.format = commonFormat;
+      julia.format = commonFormat;
+      kotlin.format = commonFormat;
+      nim.format = commonFormat;
+      nodejs.format = commonFormat;
+      opa.format = commonFormat;
+      perl.format = commonFormat;
+      php.format = commonFormat;
+      purescript.format = commonFormat;
+      red.format = commonFormat;
+      ruby.format = commonFormat;
+      rust.format = commonFormat;
+      scala.format = commonFormat;
+      swift.format = commonFormat;
     };
   };
 }
