e80e1ff586fc43bade54fc8ffef17fa34981ff86
nikmosi <i@nikmosi.ru>
2025-04-22 00:15:55 +0700

    feat(tmux): add sesh popup

--- home-manager/modules/tmux.nix
+++ home-manager/modules/tmux.nix
@@ -44,6 +44,19 @@
 
       bind -r m resize-pane -Z
 
+      bind-key "t" display-popup -yC -xR -E -b rounded -s 'bg=#1a1b26,fg=#c0caf5' -S 'fg=#7aa2f7,bg=#1a1b26' 'sesh connect (sesh list --icons | fzf
+        --no-sort --ansi --border-label " sesh " --prompt "⚡  " 
+        --header "  ^a all ^t tmux ^g configs ^x zoxide ^d tmux kill ^f find" 
+        --bind "tab:down,btab:up" 
+        --bind "ctrl-a:change-prompt(⚡  )+reload(sesh list --icons)" 
+        --bind "ctrl-t:change-prompt(🪟  )+reload(sesh list -t --icons)" 
+        --bind "ctrl-g:change-prompt(⚙️  )+reload(sesh list -c --icons)" 
+        --bind "ctrl-x:change-prompt(📁  )+reload(sesh list -z --icons)" 
+        --bind "ctrl-f:change-prompt(🔎  )+reload(fd -H -d 2 -t d -E .Trash . ~)" 
+        --bind "ctrl-d:execute(tmux kill-session -t {2..})+change-prompt(⚡  )+reload(sesh list --icons)" 
+        --preview-window "right:55%" \
+        --preview "sesh preview {}"
+      )' 
 
       set-window-option -g mode-keys vi
 
