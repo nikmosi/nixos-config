86091f4612fa9b4f3834bf5694cd0831ceba85fe
nikmosi <i@nikmosi.ru>
2025-05-07 00:59:14 +0700

    chore(mpv): chanage to fork version of recent

--- home-manager/modules/mpv.nix
+++ home-manager/modules/mpv.nix
@@ -1,8 +1,8 @@
 {
   home.file.".config/mpv/scripts/recent.lua" = {
     source = builtins.fetchurl {
-      url = "https://raw.githubusercontent.com/hacel/recent/master/recent.lua";
-      sha256 = "sha256:1kc998vyq43jijrflsp2fn79b52fp6pblr2hc01gwynd2msdki4v"; # replace this
+      url = "https://raw.githubusercontent.com/nikmosi/recent/master/recent.lua";
+      sha256 = "sha256:1kc998vyq43jijrflsp2fn79b52fp6pblr2hc01gwynd2msdki4v";
     };
   };
 }
