fd6aeb982064fc55c4918f86ecfcd491c444b376
nikmosi <i@nikmosi.ru>
2025-06-11 08:21:37 +0700

    fix(hardware): use kernel from config.boot

--- nixos/modules/hardware.nix
+++ nixos/modules/hardware.nix
@@ -1,10 +1,10 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   nixpkgs.config.allowUnfree = true;
   hardware.nvidia = {
     modesetting.enable = true;
     open = true;
-    package = pkgs.linuxPackages.nvidiaPackages.stable;
+    package = config.boot.kernelPackages.nvidiaPackages.stable;
     nvidiaSettings = true;
     powerManagement.finegrained = false;
     powerManagement.enable = false;
