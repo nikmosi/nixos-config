749bc357793917562d1b9b7a53797ec6e98ff324
nikmosi <i@nikmosi.ru>
2025-07-22 22:09:55 +0700

    feat(modules): add sshd

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -1,4 +1,15 @@
 {
+  services.openssh = {
+    enable = true;
+    ports = [ 22 ];
+    settings = {
+      PasswordAuthentication = true;
+      AllowUsers = [ "nik" ]; # Allows all users by default. Can be [ "user1" "user2" ]
+      UseDns = true;
+      X11Forwarding = false;
+      PermitRootLogin = "prohibit-password"; # "yes", "without-password", "prohibit-password", "forced-commands-only", "no"
+    };
+  };
   services.libinput = {
     enable = true;
     touchpad.accelProfile = "flat";
